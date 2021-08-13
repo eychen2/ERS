79f7888724382b7a6ccb0f1402c3d9f67efbe32e
eychen <echen963@gmail.com>
2021-08-13 15:09:42 -0400

    Wrote card constructor and accessor functions

--- card.cpp
+++ card.cpp
@@ -1,4 +1,18 @@
 //
 // Created by echen on 8/13/2021.
 //
-
+#include<string>
+#include"card.h"
+card::card(std::string suit_, int val_)
+{
+    suit=suit_;
+    val=val_;
+}
+std::string card::getSuit()
+{
+    return suit;
+}
+int card::getVal()
+{
+    return val;
+}
--- deck.h
+++ deck.h
@@ -4,5 +4,14 @@
 
 #ifndef ERS_DECK_H
 #define ERS_DECK_H
+class deck
+{
+public:
+    int size;
+    queue<Card> deck_;
+private:
+    deck(int size_);
+    void shuffle();
 
+};
 #endif //ERS_DECK_H
